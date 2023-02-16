8d3193280b6a203dc8a9bd7c31e9d8be87d0f7a5
dnlespinosa <89487129+dnlespinosa@users.noreply.github.com>
2023-02-15 20:43:06 -0700

    Add files via upload

--- companies.js
+++ companies.js
@@ -6,7 +6,7 @@ const jsonschema = require("jsonschema");
 const express = require("express");
 
 const { BadRequestError } = require("../expressError");
-const { ensureLoggedIn } = require("../middleware/auth");
+const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth");
 const Company = require("../models/company");
 
 const companyNewSchema = require("../schemas/companyNew.json");
@@ -53,45 +53,6 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
 router.get("/", async function (req, res, next) {
   try {
     const companies = await Company.findAll();
-    const returnVal = []
-    const body = req.body
-    if (Object.keys(body).length > 0) {
-      // nameLike
-      if (Object.keys(body)[0] === 'name') {
-        for (let company of companies) {
-          let objKeys = Object.keys(company)
-          for (let i=0; i<objKeys.length; i++) {
-            if (Object.keys(body).toString() === objKeys[i]) {
-              let val = Object.values(body).toString()
-              if (company[objKeys[i]].includes(val)) {
-                returnVal.push(company)
-              }
-            }
-          }
-        }
-      }
-
-      // minEmployees
-      if (Object.keys(body[0] === 'minEmployees')) {
-        for (let company of companies) {
-          if (company.numEmployees > Object.values(body).toString()) {
-            returnVal.push(company)
-          }
-        }
-      }
-
-      // maxEmployees
-      if (Object.keys(body[0] === 'maxEmployees')) {
-        for (let company of companies) {
-          if (company.numEmployees < Object.values(body).toString()) {
-            returnVal.push(company)
-          }
-        }
-      }
-    
-      return res.json({ returnVal })
-    }
-
     return res.json({ companies });
   } catch (err) {
     return next(err);
@@ -106,7 +67,7 @@ router.get("/", async function (req, res, next) {
  * Authorization required: none
  */
 
-router.get("/search/:handle", async function (req, res, next) {
+router.get("/:handle", async function (req, res, next) {
   try {
     const company = await Company.get(req.params.handle);
     return res.json({ company });
@@ -114,7 +75,6 @@ router.get("/search/:handle", async function (req, res, next) {
     return next(err);
   }
 });
-
 
 /** PATCH /[handle] { fld1, fld2, ... } => { company }
  *
@@ -127,7 +87,7 @@ router.get("/search/:handle", async function (req, res, next) {
  * Authorization required: login
  */
 
-router.patch("/:handle", ensureLoggedIn, async function (req, res, next) {
+router.patch("/:handle", ensureLoggedIn, ensureAdmin,  async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, companyUpdateSchema);
     if (!validator.valid) {
@@ -147,7 +107,7 @@ router.patch("/:handle", ensureLoggedIn, async function (req, res, next) {
  * Authorization: login
  */
 
-router.delete("/:handle", ensureLoggedIn, async function (req, res, next) {
+router.delete("/:handle", ensureLoggedIn, ensureAdmin, async function (req, res, next) {
   try {
     await Company.remove(req.params.handle);
     return res.json({ deleted: req.params.handle });
@@ -158,57 +118,3 @@ router.delete("/:handle", ensureLoggedIn, async function (req, res, next) {
 
 
 module.exports = router;
-
-const companies = [
-  {
-    "handle": "anderson-arias-morrow",
-    "name": "Anderson, Arias and Morrow",
-    "description": "Somebody program how I. Face give away discussion view act inside. Your official relationship administration here.",
-    "numEmployees": 245,
-    "logoUrl": "/logos/logo3.png"
-  },
-  {
-    "handle": "arnold-berger-townsend",
-    "name": "Arnold, Berger and Townsend",
-    "description": "Kind crime at perhaps beat. Enjoy deal purpose serve begin or thought. Congress everything miss tend.",
-    "numEmployees": 795,
-    "logoUrl": null
-  },
-  {
-    "handle": "ayala-buchanan",
-    "name": "Ayala-Buchanan",
-    "description": "Make radio physical southern. His white on attention kitchen market upon. Represent west open seven. Particularly subject billion much score thank bag somebody.",
-    "numEmployees": 309,
-    "logoUrl": null
-  },
-  {
-    "handle": "baker-santos",
-    "name": "Baker-Santos",
-    "description": "Compare certain use. Writer time lay word garden. Resource task interesting voice.",
-    "numEmployees": 225,
-    "logoUrl": "/logos/logo3.png"
-  },
-  {
-    "handle": "bauer-gallagher",
-    "name": "Bauer-Gallagher",
-    "description": "Difficult ready trip question produce produce someone.",
-    "numEmployees": 862,
-    "logoUrl": null
-  }
-]
-
-let body = {name: 'ow'}
-let returnName = []
-
-
-for (let company of companies) {
-  let objKeys = Object.keys(company)
-  for (let i=0; i<objKeys.length; i++) {
-    if (Object.keys(body).toString() === objKeys[i]) {
-      let val = Object.values(body).toString()
-      if (company[objKeys[i]].includes(val)) {
-        returnName.push(company[objKeys[i]])
-      }
-    }
-  }
-}
